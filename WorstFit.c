9457301a0de2ed07b23444d86d3281656cf3ce08
Giuseppe Fiorentino Neto <fiorentinogiuseppebcc@gmail.com>
2019-04-30 09:32:06 -0300

    refeito, pois da forma que tava so atrapalhava
    Totalmente teste e sem organização, mas to apressado kkkk

--- WorstFit.c
+++ WorstFit.c
@@ -1,124 +1,321 @@
-//Resumo
+#include<unistd.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <pthread.h> 
 
-//The Worst Fit Memory alloc Algorithm
-//allocates the largest free partition avai-
-//lable in the memory that is sufficient
-//enough to hold the process within the
-//system.
+//gcc -w -pthread wf.c -o wf && ./wf
 
-//It searches the complete memory for
-//available free partitions and allocates
-//the process to the memory partition
-//which is the largest out of all.
+typedef struct candidatos
+{
+  int tam;
+  int flag;                      
+  void *ptr;           
+}candidatos;
 
-//This algorithm is not recommended to be
-//implemented in the real world as it has
-//many disadvantages.
+typedef struct memoryList
+{
+  // doubly-linked list
+  struct memoryList *last;
+  struct memoryList *next;
 
-//A process entering first may be allocated
-//the largest memory space but if another
-//process of larger memory requirement is
-//to be allocated, space cannot be found.
-//This is a serious drawback here.
+  unsigned tam;
+  unsigned alloc;                      
+  void *ptr;           
+}memoryList;
 
-//===============================================
+unsigned tamMem;
+void *myMemory = NULL;
 
-//Include
-#include<stdio.h>
+static memoryList *head;
+static memoryList *next;
 
-void worstFit(int* tamBlock, int nblock, int* tamProc, int nArq ){
-	//Onde sera guardado os Ids dos processos
-	int alloc[nArq];
 
-	//Inicializando com -1, pois não há o mapeamento bloco x processo 
-	for(int i =0 ; i< nArq; i++) alloc[i]=-1;
+memoryList* worst_block(unsigned req) {
 
-	//prints antes para mostrar
-	printf(">>>>>>>>>>ANTES<<<<<<<<<<\n");
-	printf("\nProcess No.\tProcess Size\tBlock no.\tBlock Size\n");
-	for(int i=0; i<nblock; i++){
-		if(i+1>nArq) printf("  %d \t\t %d \t\t  %d \t\t  %d \t\t \n",-1,-1,-1,tamBlock[i]);
-		else printf("  %d \t\t %d \t\t  %d \t\t  %d \t\t \n",i+1,tamProc[i],-1,tamBlock[i]);
-			
+	memoryList* current = head, *max = NULL;
 
-	}
-	
-	//Rodando o Algoritmo
-	printf("\n\n>>>>>>>>>>RODANDO<<<<<<<<<<\n\n");
-	//Pega cada professo e encontra um bloco para ele
-	//De acordo com o seu tamanho
-	int n = 0; //variavel so par ajudar na hora de printar ja q não foi usado processo 0-(n-1) sim 1-n
-        for(int i=0; i<nArq;i++){
-		int n=i+1;
-		printf("\n\nProcess No.  %d \t\t \n", n);
-		//O index do melhor bloco do processo atual
-		int wstId = -1;
-		//encontra o real posicionamento do  
-                for(int j=0; j<nblock; j++){
-			printf("\nEncontrando melhor bloco para o Process No. %d... \t\t\n", n);
-			//vai encontrando o posicionamento
-			printf("Block no.  %d \t\t \n",j);
-			if(tamBlock[j] >= tamProc[i] ){
-				printf("Process No. %d = %d cabe no Block No. %d = %d\n",n,tamProc[i],j,tamBlock[j] );
-				//Se o index for -1 não tem ninguem é so alterar
-				if(wstId == -1) {
-					printf("Block No. %d = %d Vazio... Alocando Process No. %d = %d... \n",j,tamBlock[j],n,tamProc[i]);
-					wstId = j;
-				}
-				//Foi salvo o idx do maior block anterior mente, porém é verificado se esse local realmente é o maior
-				//Coso não for é substituido o wsId pelo maior caso contrario, como entrou no else if não possui espaço pra ele 
-				else if (tamBlock[wstId] < tamBlock[j]){
-					printf("Bloco No. %d = %d alocado, porem possui espaco para o Process No. %d = %d...\n",j,tamBlock[j],n,tamProc[i]);
-					wstId = j;
-				}
-				else
-					printf("Bloco No. %d = %d já alocado e nao possui espaco para o Process No. %d = %d...\n",j,tamBlock[j],i,tamProc[i]);
-                        }
-			else{
-				printf("Process No. %d = %d nao cabe no Block No. %d = %d\n",n,tamProc[i],j,tamBlock[j] );
-			}
-                }
-
-		//Se foi encontrado a localização
-		if(wstId != -1){
-			//Alocamos o bloco j para o processo p[i]
-			alloc[i] = wstId;
-			//Diminui o tamanho  dql bloco
-			tamBlock[wstId] -= tamProc[i];
-			printf("\n\n\nEncontrado o Bloco No. %d para o Process No. %d = %d\t\t\n", wstId,n,tamProc[i]);
-			printf("Tamanho do Bloco No. %d atual eh: %d\n", wstId,tamBlock[wstId]);
+	do {//aq eh lista circular current != head
+		if(!(current->alloc) && (!max || current->tam > max->tam) ) {
+			max = current;
 		}
-		else {
-			printf("\n\n\nNao encontrado um bloco para o Process No. %d = %d\t\t\n", n,tamProc[i]);
-			printf("Tamanho do Bloco No. %d atual eh: %d\n", wstId,tamBlock[wstId]);
-		}
-        }
-	printf("\n\n>>>>>>>>>>FIM<<<<<<<<<<\n\n");
-
-	//prints para mostrar
-	printf("\n\n>>>>>>>>>>DEPOIS<<<<<<<<<<\n\n");
-	printf("\nProcess No.\tProcess Size\tBlock no.\tBlock Size\n");
-	for(int i=0; i<nArq; i++){
-		printf("  %d \t\t %d \t\t",i+1,tamProc[i]);
-		if(alloc[i] != -1)
-			printf("%i \t\t ", alloc[i]+1);
-		else
-			printf("-1 \t\t ");
-		printf("  %d \t\t \n",tamBlock[i]);
-
+		current = current->next;
+	}while(current != head); 
+	if(max->tam >= req) {
+		return max;
+	} else {
+		return NULL;
 	}
-
-
 }
 
-//alterar o nome nblock e nArq
-int main(void){
-	int blockSize[] = {100, 500, 200, 300, 600}; 
-	int processSize[] = {212, 417, 112, 426}; 
-	int nblock = sizeof(blockSize)/sizeof(blockSize[0]); 
-	int nArq = sizeof(processSize)/sizeof(processSize[0]); 
+void startUpMem(unsigned sz) {
   
-	worstFit(blockSize, nblock, processSize, nArq);
-	return 0;
+	tamMem = sz;
+
+	/* Initialize memory management structures */
+	myMemory = malloc(sz);
+	head = malloc(sizeof(memoryList));
+	head->tam = sz;
+	head->alloc = 0;
+	head->ptr = myMemory;
+	next = head;
+
+	head->last = head;
+	head->next = head;
 }
+
+
+void *alloc(unsigned req) {
+	memoryList* block = worst_block(req);
+
+	if(!block) {
+		printf("ERROR SEM MEMORIA REAL...\n");
+		return NULL;
+	}
+
+	if(block->tam > req) {
+		// Concontainer for unallocated remainder of this block 
+		memoryList* remainder = malloc(sizeof(memoryList));
+    	
+		// Insert into linked list 
+
+		remainder->next = block->next;
+
+
+
+		remainder->next->last = remainder;
+
+		remainder->last = block;
+
+		block->next = remainder;
+    
+		// Divide up allocated memory 
+		remainder->tam = block->tam - req;
+		remainder->alloc = 0;
+		remainder->ptr = block->ptr + req;
+		block->tam = req;
+		next = remainder;
+	} else {
+		next = block->next;
+	}
+  
+	block->alloc = 1;
+  
+	//Return pointer to the allocated block 
+	return block->ptr;
+
+}
+
+
+void contiguous_block_prev( memoryList* current ){
+	if(current != head && !(current->last->alloc)) {
+		memoryList* prev = current->last;
+		prev->next = current->next;
+		prev->next->last = prev;
+		prev->tam += current->tam;
+    
+		if(next == current) {
+			next = prev;
+		}
+    
+		free(current);
+		current = prev;
+	}
+}
+
+
+void contiguous_block_next( memoryList* current ){
+	  if(current->next != head && !(current->next->alloc)) {
+		struct memoryList* second = current->next;
+		current->next = second->next;
+		current->next->last = current;
+		current->tam += second->tam;
+    
+    		if(next == second) {
+			next = current;
+		}
+    
+	    	free(second);
+	}
+
+}
+
+/* Frees a block of memory previously allocated by alloc. */
+void letfree(void* block) {
+	/* Iterate over memory list, searching for the target block's container */
+	printf("LIBERANDO BLOCO %p...\n",block);
+	struct memoryList* current = head;
+	do {
+		printf("VERIFICANDO BLOCO %p...\n",block);
+		if(current->ptr == block) {
+			printf("ACHADO O BLOCO %p...\n",block);
+			break;
+		}
+	} while((current = current->next) != head);
+
+	/* Flag this block as freed */
+	current->alloc = 0;
+
+	contiguous_block_prev(current);
+	contiguous_block_next(current);
+}
+
+
+
+
+int length() {
+	int length = 0;
+	memoryList *current = head;
+	
+	do{
+		length++;
+		current = current->next;
+	} while(current != head); 
+	
+   return length;
+}
+void sort() {
+   int i, j, k, tempAlloc, tempTam;
+   int *tempPtr;
+   memoryList *current;
+   memoryList *next;
+	
+   int size = length();
+   k = size ;	
+	
+   for ( i = 0 ; i < size - 1 ; i++, k-- ) {
+      current = head;
+      next = head->next;
 		
+      for ( j = 1 ; j < k ; j++ ) { 
+         if ( current->alloc > next->alloc ) {
+            tempAlloc = current->alloc;
+            current->alloc = next->alloc;
+            next->alloc = tempAlloc;
+
+            tempTam = current->tam;
+            current->tam = next->tam;
+            next->tam = tempTam;
+	    
+	    tempPtr = current->ptr;
+            current->ptr = next->ptr;
+            next->ptr = tempPtr;
+
+         }
+			
+         current = current->next;
+         next = next->next;
+	 
+      }
+   } 
+	  
+}
+
+void compact(){
+	memoryList *current = head;
+	sort();
+	do{
+		contiguous_block_next(current);		
+		current = current->next;
+	} while(current != head); 
+
+}
+
+
+/* Use this function to print out the current contents of memory. */
+void print_memory() {
+  printf("Memory List {\n");
+  /* Iterate over memory list */
+  struct memoryList* index = head;
+  do {
+    printf("\tBlock %p,\tsize %d,\t%s\n",
+           index->ptr,
+           index->tam,
+           (index->alloc ? "[ALLOCATED]" : "[FREE]"));
+  } while((index = index->next) != head);
+  printf("}\n");
+}
+int somatorio(int  arr[], int n){
+	int sum=0;
+	for (int i = 0; i < n; i++)
+	 {
+	    sum = sum + arr[i];
+	 }
+	return sum;
+}
+
+
+void mymem() {
+	memoryList *a=NULL;
+	memoryList *arr[5]; //array de ponteiros des elementos
+
+	for (int i = 0; i < 5; i++) {
+		arr[i] = a;
+	}
+
+	int pF, vF; //variaveis aleatorias para acessar o array
+	int tam;
+	int candidatos[] ={50,200,300,400,500,600};
+	int flag[]={0,0,0,0,0,0};
+	int numCandidatos=6;
+	int cont=0; //contador
+	int memRemain=0, memInit; //quanto de memoria ainda tem q usar usar 
+	
+	memRemain= somatorio(candidatos,numCandidatos);
+	memInit=memRemain;
+	tamMem=520;
+	startUpMem(tamMem);
+	while(memRemain>0){
+
+		print_memory();
+		tam=candidatos[cont];
+		printf("CONT %i\n\n\n", cont);
+		printf("TAMANHO %i\n\n\n",length());
+		if(tam>tamMem){
+			printf("ERROR TAMANHO %i MUITO GRANDE PARA A MEMORIA. NAO CONSEGUIRA SER ALOCADO\n",tam);
+			candidatos[cont]=0;
+		}
+		else if(tam<=0)
+			printf("ERROR VALOR %i INVALIDO ou 0\n",tam);
+		else{
+			if(tam>0){
+				printf("TENTANDO ALOCAR %i...\n",tam);
+				arr[cont] = alloc(tam);
+				if(arr[cont]!=NULL){
+					printf("ALOCADO %i...\n",tam);
+					candidatos[cont]=0;
+					flag[cont]=1;
+					print_memory();
+				}
+			}
+		}
+		if(memRemain<memInit){
+			pF=rand()%2;
+			printf("PF = %i\n",pF);
+			if(pF == 1){
+				vF=rand() % numCandidatos;
+				printf("VF = %i \n",vF);
+				printf("Flag = %i\n", flag[vF]);
+				printf("candidato: %i\n", candidatos[vF]);
+				if(candidatos[vF]==0 && flag[vF]==1){
+					printf("LIBERANDO %p...\n", arr[vF]);
+					letfree(arr[vF]);
+					compact();
+					flag[vF]=0;
+					print_memory();
+				}
+			}
+		}
+		memRemain=somatorio(candidatos,numCandidatos);
+		cont++;
+		if(cont>=numCandidatos)	cont=0;
+		sleep(3);
+	}
+}
+
+int main(){
+	mymem();
+	return 0;
+
+}
