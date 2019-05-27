b051451e269eaf6ebd0be7f263ac1d8ddeba87ce
Giuseppe Fiorentino Neto <fiorentinogiuseppebcc@gmail.com>
2019-05-27 09:20:44 -0300

    modificar

--- fs.h
+++ fs.h
@@ -1,10 +1,106 @@
-#include "ext2.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdint.h>
+//Valores default
+#define EXT2_NAME_LENGTH 15
+#define EXT2_PTR_LENGTH 8
+#define EXT2_PAD_LENGTH 10
+#define BLOCKS 4611
+#define BLOCK_SIZE 1024
+#define INODE_SIZE 64
+#define DATA_BEGIN_BLOCK 515
+#define DIR_SIZE 32
+#define DIRECT_INDEX_LIMIT 3072
+#define DIRECT_INDEX_BLOCKS 6
+#define ONE_INDEX_LIMIT 131072
+#define ONE_INDEX_BLOCKS 256
+#define INDEX_LIMIT 2097152 // 4096*512
+#define INDEX_BLOCKS 4096
+#define PATH "vDisk"
+#define COMMAND_SIZE 10
 
+#define FORMAT 0
+#define LS 2
+#define CREATE 3
+#define DELETE 4
+#define CD 5
+#define CLOSE 6
+#define READ 7
+#define WRITE 8
+#define EXIT 9
+
+char *commands[] = {
+    "format",
+    "password",
+    "ls",
+    "create",
+    "delete",
+    "cd",
+    "close",
+    "read",
+    "write",
+    "exit"
+};
+
+/***********INODE***********/
+/*
+ * Special numbers
+ */
+#define FILE_TYPE_FILE 1
+#define FILE_TYPE_DIR 2
+
+/*
+	Algumas definições
+	http://www.science.unitn.it/~fiorella/guidelinux/tlk/node95.html
+*/
+
+
+//Physical Layout of the EXT2 File system
+//bg de block group
+//Bitmap = Keeps track of which blocks are free/used
+typedef struct mygroup_descriptor
+{
+    char bg_volume_name[EXT2_NAME_LENGTH + 1];          /* Nome do "volume"*/
+    uint16_t bg_block_bitmap;        			/* Blocks bitmap block */ 
+    uint16_t bg_inode_bitmap;        			/* Inodes bitmap block */ 
+    uint16_t bg_inode_table;         			/* Inodes table block */
+    uint16_t bg_free_blocks_count;   			/* Free blocks count */
+    uint16_t bg_free_inodes_count;   			/* Free inodes count */
+    uint16_t bg_used_dirs_count;     			/* Directories count */
+} group_descriptor;
+
+//EXT2 Inode
+
+typedef struct myinode {
+    time_t tmpa;                  // data de acesso
+    time_t tmpc;                  // data criação
+    time_t tmpm;                  // delete modificacao
+    time_t tmpd;                  // delete time
+    uint16_t mod;                 //  tipo de arquivo e direitos de acesso
+    uint16_t nblock;               // número de blocos de dados do arquivo
+    uint16_t tblock;                 // tamanho do arquivo
+    uint16_t ptrblock[EXT2_PTR_LENGTH];             // ponteiro para o bloco de dados
+    char i_pad[EXT2_PAD_LENGTH];                  // preenchimento 
+} inode;
+
+/*
+ * Structure of a directory entry
+ */
+typedef struct mydir_entry {
+    uint16_t inod;                  // index number
+    uint16_t rec_len;                // comprimento do item de diretório
+    uint16_t name_len;               // comprimento da pasta
+    uint16_t file_type;              // file type 1 diretório do arquivo 2
+    char name[EXT2_NAME_LENGTH];   // nome do arquivo
+    char dir_pad[EXT2_PAD_LENGTH-1];                 // preenchimento
+} dir_entry;
+
+
+
+/***********FS***********/
 // global var 
 group_descriptor group_desc;
 
@@ -69,7 +165,7 @@ int fs_init() {
 	// Verifica se o sistema de arquivos existe 
 	int retorno = fs_check();
 	//Se o retorno
-	if (fs_check < 0) {
+	if (retorno < 0) {
         	//Criando o sistema de arquivo
 		formatacao();
 		load_global();
@@ -615,7 +711,7 @@ int dir_root_create() {
     }
     inode root_inode;
     inodeInit(&root_inode);
-    dir_info root_dir_entry;
+    dir_entry root_dir_entry;
     time_t now;
     time(&now);
     root_inode.tmpa = now;
@@ -626,7 +722,7 @@ int dir_root_create() {
     root_inode.nblock = 0;
     root_inode.tblock = 0;
     inodeWrite(inode_id, &root_inode);
-    root_dir_entry.inode = inode_id;
+    root_dir_entry.inod = inode_id;
     root_dir_entry.rec_len = DIR_SIZE;
     root_dir_entry.name_len = 1;
     root_dir_entry.file_type = FILE_TYPE_DIR;
