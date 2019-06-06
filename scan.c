56d357da37b2c2b0f8bde74278f90114ef868512
Giuseppe Fiorentino Neto <fiorentinogiuseppebcc@gmail.com>
2019-06-06 15:23:25 -0300

    Update scan.c

--- scan.c
+++ scan.c
@@ -5,20 +5,20 @@ it scans up servicing the requests that it didn't get going down.
 If a request comes in after it has been scanned it will not be serviced
 until the process comes back down or moves back up.
 */
-//print printar as extremidades como campo de visita
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #define LOW 0
 #define HIGH 199
 
-
+int soma(int a, int b){ int c=a+b; return c;}
 int main(){
   int queue[20];
   int head, max, q_size, temp, sum;
   int dloc; //location of disk (head) arr
 
-
+  /*
   printf("%s\t", "Input no of disk locations");
   scanf("%d", &q_size);
 
@@ -29,7 +29,17 @@ int main(){
   for(int i=0; i<q_size; i++){
     scanf("%d", &queue[i]);
   }
-
+  */
+  q_size=8;
+  head=53;
+  queue[0]=98;
+  queue[1]=183;
+  queue[2]=41;
+  queue[3]=122;
+  queue[4]=14;
+  queue[5]=124;
+  queue[6]=65;
+  queue[7]=67;
   queue[q_size] = head; //add RW head into queue
   q_size++;
 
@@ -53,32 +63,64 @@ int main(){
       break;
     }
   }
-
+  int anterior=head;
+  int movimentos=0;
   if(abs(head-LOW) <= abs(head-HIGH)){
-
+     printf("\nSubindo...\n");
+     movimentos+=abs(anterior-head);
+     printf("(%d-%d) + ",head,anterior);
+     for(int j=dloc+1; j<=q_size; j++){
+        
+        if(j==q_size){
+          //printf("%d --> ",HIGH);
+          movimentos+=abs(anterior-HIGH);
+          printf("(%d-%d) + ",HIGH,anterior);
+          anterior=HIGH;
+        }
+        else{
+          //printf("%d --> ",queue[j]);
+          movimentos+=abs(anterior-queue[j]);
+          printf("(%d-%d) + ",queue[j],anterior);
+          anterior=queue[j];
+        }
+        
+      }
+      printf("\nMovimentos = %d\n",movimentos);
+      printf("\nDescendo...\n");
       for(int j=dloc; j>=0; j--){
+          
+        if(j==q_size){
+          //printf("%d --> ",LOW);
+          movimentos+=abs(anterior-LOW);
+          printf("(%d-%d) + ",LOW,anterior);
+          anterior=LOW;
+        }
+        else if(j!=dloc){
+          //printf("%d --> ",queue[j]);
+          movimentos+=abs(anterior-queue[j]);
+          printf("(%d-%d) + ",queue[j],anterior);
+          anterior=queue[j];
+        }
+      }
+            printf("\nMovimentos = %d\n",movimentos);
+
+
+  } else {
+for(int j=dloc; j>=0; j--){
         printf("%d --> ",queue[j]);
       }
       for(int j=dloc+1; j<q_size; j++){
         printf("%d --> ",queue[j]);
       }
-
-      } else {
-
-      for(int j=dloc+1; j<q_size; j++){
-          printf("%d --> ",queue[j]);
-      }
-      for(int j=dloc; j>=0; j--){
-          printf("%d --> ",queue[j]);
-      }
+     
 
   }
 
 
 
 
-  sum  = head + max;
-  printf("\nmovement of total cylinders %d", sum);
+  
+  printf("\nmovement of total cylinders %d", movimentos);
 
   return 0;
 }
